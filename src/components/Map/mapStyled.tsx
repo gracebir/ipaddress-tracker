2c61ae54880e401c0d040ec24e8d611b595ceef0
gracebir <grace.devolop@gmail.com>
2023-02-27 19:48:34 +0200

    changing the height of the map

--- src/components/Map/mapStyled.tsx
+++ src/components/Map/mapStyled.tsx
@@ -3,8 +3,7 @@ import styled from "styled-components";
 export const MapContainer = styled.div`
   height: 60vh;
   width: 100%;
-  background-color: #0303;
   @media (min-width: 45rem) {
-    height: 70vh;
+    height: 65vh;
   }
 `;
